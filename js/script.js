df2e4d588337c5d475c8509cb24a5c9405927ffa
Maverick1026 <mhdeclaro.student@asiancollege.edu.ph>
2023-11-13 11:42:43 +0800

    adjusting some offsets

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Get a reference to the "Best Sellers" dropdown item and menu
 const bestSellerDropdownItem = document.querySelector('.nav-item.dropdown .nav-link.dropdown-toggle');
 const bestSellerDropdownMenu = document.querySelector('.nav-item.dropdown .dropdown-menu');
 
@@ -9,16 +10,15 @@ bestSellerDropdownItem.addEventListener('click', (event) => {
     // Get the href attribute of the "Best Sellers" dropdown item
     const bestSellerHref = bestSellerDropdownItem.getAttribute('href');
 
-    // Scroll to the target section
-    document.querySelector(bestSellerHref).scrollIntoView({
-        behavior: 'smooth'
-    });
+    // Scroll to the target section with an offset
+    scrollToSection(bestSellerHref, -50); // Adjust the offset as needed
 
     // Close the dropdown menu if it's open
     if (bestSellerDropdownMenu.classList.contains('show')) {
         bestSellerDropdownMenu.classList.remove('show');
     }
 });
+
 // Get a reference to the "Home" link
 const homeLink = document.querySelector('a[href="#home"]');
 
@@ -37,67 +37,67 @@ const galleryLink = document.querySelector('a[href="#gallery"]');
 const blogLink = document.querySelector('a[href="#blog"]');
 const blogLinkMenu = document.querySelector('section#about a[href="#blog"]');
 
-
 // Get a reference to the "Contact" link
 const contactLink = document.querySelector('a[href="#contact"]');
 
 // Get the height of the header to offset the scroll position
 const headerHeight = document.querySelector('.header').offsetHeight;
 
-// Function to handle smooth scrolling
-function scrollToSection(sectionId) {
-  window.scrollTo({
-    top: document.querySelector(sectionId).offsetTop - headerHeight,
-    behavior: 'smooth',
-  });
+// Function to handle smooth scrolling with an optional offset
+function scrollToSection(sectionId, offset = 0) {
+    window.scrollTo({
+        top: document.querySelector(sectionId).offsetTop - headerHeight + offset,
+        behavior: 'smooth',
+    });
 }
 
 // Add a click event listener to the "Home" link
 homeLink.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#home');
+    event.preventDefault();
+    scrollToSection('#home', -1000);
 });
 
 // Add a click event listener to the "About Us" link within the home section
 aboutLinkHome.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#about');
+    event.preventDefault();
+    scrollToSection('#about', 20); // Adjust the offset as needed
 });
 
 aboutLink.addEventListener('click', (event) => {
     event.preventDefault();
-    scrollToSection('#about');
-  });
-  
+    scrollToSection('#about', 20); // Adjust the offset as needed
+});
+
 // Add a click event listener to the "Menu" link
 menuLink.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#menu');
+    event.preventDefault();
+    scrollToSection('#menu', 30); // Adjust the offset as needed
 });
+
 menuLinkMenu.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#menu');
+    event.preventDefault();
+    scrollToSection('#menu', 30); // Adjust the offset as needed
 });
 
 // Add a click event listener to the "Gallery" link
 galleryLink.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#gallery');
+    event.preventDefault();
+    scrollToSection('#gallery', 40); // Adjust the offset as needed
 });
 
 // Add a click event listener to the "Blog" link
 blogLink.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#blog');
+    event.preventDefault();
+    scrollToSection('#blog', 50); // Adjust the offset as needed
 });
 
 blogLinkMenu.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#blog');
+    event.preventDefault();
+    scrollToSection('#blog', 50); // Adjust the offset as needed
 });
 
 // Add a click event listener to the "Contact" link
 contactLink.addEventListener('click', (event) => {
-  event.preventDefault();
-  scrollToSection('#contact');
+    event.preventDefault();
+    scrollToSection('#contact', 60); // Adjust the offset as needed
 });
